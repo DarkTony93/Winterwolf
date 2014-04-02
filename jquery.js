a7e8431e3892bd47fe5c09588622a65fc82df651
Antonio <Ciccarelli>
2014-04-02 21:22:36 +0200

    Aggiunta File Progetto

---
+++ jquery.js
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<script type = "text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+		</script>
+	
+	<script>
+		$(document).ready(function()
+		{	
+			$("#anima1").fadeIn(8000);
+			$("#credit2").fadeOut(3000, function());
+			$("#credit2").fadeOut(8000);	
+		});
+	</script>	
+	
+	</head>
